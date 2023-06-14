84d5960084e2c2ecf671d6a4adb9c3da259eb93e
Haoliang Zheng zheng.1947@buckeyemail.osu.edu <zheng@cms3.physics.ucsb.edu>
2023-06-13 18:11:00 -0700

    update learn

--- binLearn.C
+++ binLearn.C
@@ -1,28 +1,59 @@
 //learn bin
+//In TH2, the (0,0) is at the lower left corner.
+
 #include <iostream>
 void binLearn()
 {
     // Create a histogram
-   TH2F *hist = new TH2F("hist", "Example Histogram", 100, 0, 100, 100, 0, 100);
+   TH2F *hist = new TH2F("hist", "Example Histogram", 100, -10, 10, 100, -10, 10);
 
    // Fill the histogram with some values
-   hist->Fill(10,10);
-   hist->Fill(20,20);
-   hist->Fill(30,30);
-   hist->Fill(40,40);
-   hist->Fill(50,50);
+   hist->Fill(1,1);
+   hist->Fill(2,2);
+   hist->Fill(3,3);
+   hist->Fill(4,4);
+   hist->Fill(5,5);
 
    // Get the bin number corresponding to a value
-   double value = 25;
-   int binX = hist->GetXaxis()->FindBin(value);
-   int binY = hist->GetYaxis()->FindBin(51);
+
+   //find the center position
+   double Xvalue = 0;
+   double Yvalue = 0;
+   int binX = hist->GetXaxis()->FindBin(Xvalue);
+   int binY = hist->GetYaxis()->FindBin(Yvalue);
    //double bincenter = hist->GetXaxis()->GetBinCenter(binx);
-   int binxy = hist->GetBinContent(31,31);
+   int binxy = hist->GetBinContent(0,0);
 
    // Print the bin number
-   cout << "Value " << value << " is in binX " << binX << endl;
-   cout << "Value " << value << " is in binY " << binY << endl;
+   cout << "Value " << Xvalue << " is in binX " << binX << endl;
+   cout << "Value " << Yvalue << " is in binY " << binY << endl;
    cout << "binxy:" << binxy << endl;
+   
+   //create a rectangle center at (0,0)
+   //the length is L=2a, and the width is W=2b.
+   // 
+   double a = 5;
+   double b = 4;
+
+   //upper bound
+   int Lp = hist->GetXaxis()->FindBin(a);
+   int Wp = hist->GetYaxis()->FindBin(b);
+   //lower bound
+   int Lpp = hist->GetXaxis()->FindBin(-a);
+   int Wpp = hist->GetYaxis()->FindBin(-b);
+
+
+   int Tbinxy = 0;
+   for (int x = Lpp; x < Lp; x++)
+   {
+     for (int y= Wpp; y < Wp; y++)
+     {
+        binxy = hist->GetBinContent(x,y);
+        Tbinxy = Tbinxy + binxy;
+     }
+   }
+
+   cout << Tbinxy << endl;
 
 
    // Access the bin content
